ea95b8121bbaf63b70a7982fbe6d7a0d314c2b8d
Andre van Zuydam <andrevanzuydam@gmail.com>
2022-05-25 00:56:48 +0200

    Added shortname for database migrations Updated packages

--- Tina4/DataMongoDb.php
+++ Tina4/DataMongoDb.php
@@ -200,7 +200,7 @@ class DataMongoDb implements Database
      * Get a short name for the database used for specific database migrations
      * @return string
      */
-    public function getShortName(): string
+    final public function getShortName(): string
     {
         return "mongodb";
     }
